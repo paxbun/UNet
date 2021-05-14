f7fe3a8089248226bb99e9d783f978e83d819c49
Chanjung Kim <freiyer.paxbun@gmail.com>
2021-05-15 03:02:25 +0900

    Fix padding issues

--- unet/layers.py
+++ unet/layers.py
@@ -14,22 +14,28 @@ class Encoder(tf.keras.layers.Layer):
     def __init__(self, num_filters: int):
         super(Encoder, self).__init__()
         self.num_filters = num_filters
+        self.padding = tf.keras.layers.ZeroPadding2D((2, 2))
         self.conv = tf.keras.layers.Conv2D(
             num_filters, kernel_size=(5, 5), strides=(2, 2))
         self.norm = tf.keras.layers.BatchNormalization()
         self.relu = tf.keras.layers.LeakyReLU(0.2)
 
     def call(self, inputs):
-        output = self.conv(inputs)
+        output = self.padding(inputs)
+        output = self.conv(output)
         output = self.norm(output)
         output = self.relu(output)
         return output
 
     def compute_output_signature(self, input_signature: tf.TensorSpec) -> tf.TensorSpec:
-        return self.conv.compute_output_signature(input_signature)
+        output = self.padding.compute_output_signature(input_signature)
+        output = self.conv.compute_output_signature(output)
+        return output
 
     def compute_output_shape(self, input_shape):
-        return self.conv.compute_output_shape(input_shape)
+        output = self.padding.compute_output_shape(input_shape)
+        output = self.conv.compute_output_shape(output)
+        return output
 
     def get_config(self):
         return {"num_filters": self.num_filters}
@@ -44,11 +50,11 @@ class Decoder(tf.keras.layers.Layer):
     """
 
     def __init__(self, num_filters: int, has_dropout: bool):
-        super(Encoder, self).__init__()
+        super(Decoder, self).__init__()
         self.num_filters = num_filters
-        self.concat = tf.keras.layers.Concatenate(axis=1)
         self.dconv = tf.keras.layers.Conv2DTranspose(
             num_filters, kernel_size=(5, 5), strides=(2, 2))
+        self.crop = tf.keras.layers.Cropping2D(((2, 1), (2, 1)))
         self.norm = tf.keras.layers.BatchNormalization()
         self.relu = tf.keras.layers.ReLU()
         self.has_dropout = has_dropout
@@ -56,18 +62,23 @@ class Decoder(tf.keras.layers.Layer):
             self.dropout = tf.keras.layers.Dropout(0.5)
 
     def call(self, inputs):
-        inputs = self.concat(inputs)
         output = self.dconv(inputs)
+        output = self.crop(output)
         output = self.norm(output)
         output = self.relu(output)
-        output = self.dropout(output)
+        if self.has_dropout:
+            output = self.dropout(output)
         return output
 
     def compute_output_signature(self, input_signature: tf.TensorSpec) -> tf.TensorSpec:
-        return self.dconv.compute_output_signature(input_signature)
+        output = self.dconv.compute_output_signature(input_signature)
+        output = self.crop.compute_output_signature(output)
+        return output
 
     def compute_output_shape(self, input_shape):
-        return self.dconv.compute_output_shape(input_shape)
+        output = self.dconv.compute_output_shape(input_shape)
+        output = self.crop.compute_output_shape(output)
+        return output
 
     def get_config(self):
         return {
